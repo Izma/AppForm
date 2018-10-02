a0b41de1eeedb7d925b43e92f6f3b644da902ff6
Ismael del Carmen López Aguilar <ismael.lopez.aguilar@outlook.com>
2018-10-01 21:36:14 -0500

    Add project files.

---
+++ AppForm/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace AppForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MenuLeft = new System.Windows.Forms.Panel();
+            this.Header = new System.Windows.Forms.Panel();
+            this.BtnMaximize = new System.Windows.Forms.PictureBox();
+            this.BtnMinimize = new System.Windows.Forms.PictureBox();
+            this.BtnMin = new System.Windows.Forms.PictureBox();
+            this.BtnClose = new System.Windows.Forms.PictureBox();
+            this.CollapseButton = new System.Windows.Forms.PictureBox();
+            this.Container = new System.Windows.Forms.Panel();
+            this.Header.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMaximize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMinimize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CollapseButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MenuLeft
+            // 
+            this.MenuLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(224)))));
+            this.MenuLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuLeft.Location = new System.Drawing.Point(0, 0);
+            this.MenuLeft.Name = "MenuLeft";
+            this.MenuLeft.Size = new System.Drawing.Size(250, 349);
+            this.MenuLeft.TabIndex = 0;
+            // 
+            // Header
+            // 
+            this.Header.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Header.Controls.Add(this.BtnMaximize);
+            this.Header.Controls.Add(this.BtnMinimize);
+            this.Header.Controls.Add(this.BtnMin);
+            this.Header.Controls.Add(this.BtnClose);
+            this.Header.Controls.Add(this.CollapseButton);
+            this.Header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Header.Location = new System.Drawing.Point(250, 0);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(550, 50);
+            this.Header.TabIndex = 1;
+            // 
+            // BtnMaximize
+            // 
+            this.BtnMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnMaximize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnMaximize.Image = ((System.Drawing.Image)(resources.GetObject("BtnMaximize.Image")));
+            this.BtnMaximize.Location = new System.Drawing.Point(484, 9);
+            this.BtnMaximize.Name = "BtnMaximize";
+            this.BtnMaximize.Size = new System.Drawing.Size(24, 24);
+            this.BtnMaximize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BtnMaximize.TabIndex = 2;
+            this.BtnMaximize.TabStop = false;
+            this.BtnMaximize.Click += new System.EventHandler(this.BtnMaximize_Click);
+            // 
+            // BtnMinimize
+            // 
+            this.BtnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnMinimize.Image = ((System.Drawing.Image)(resources.GetObject("BtnMinimize.Image")));
+            this.BtnMinimize.Location = new System.Drawing.Point(484, 9);
+            this.BtnMinimize.Name = "BtnMinimize";
+            this.BtnMinimize.Size = new System.Drawing.Size(24, 24);
+            this.BtnMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BtnMinimize.TabIndex = 3;
+            this.BtnMinimize.TabStop = false;
+            this.BtnMinimize.Click += new System.EventHandler(this.BtnMinimize_Click);
+            // 
+            // BtnMin
+            // 
+            this.BtnMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnMin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnMin.Image = ((System.Drawing.Image)(resources.GetObject("BtnMin.Image")));
+            this.BtnMin.Location = new System.Drawing.Point(454, 9);
+            this.BtnMin.Name = "BtnMin";
+            this.BtnMin.Size = new System.Drawing.Size(24, 24);
+            this.BtnMin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BtnMin.TabIndex = 4;
+            this.BtnMin.TabStop = false;
+            this.BtnMin.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClose.Image = ((System.Drawing.Image)(resources.GetObject("BtnClose.Image")));
+            this.BtnClose.Location = new System.Drawing.Point(514, 9);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(24, 24);
+            this.BtnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BtnClose.TabIndex = 1;
+            this.BtnClose.TabStop = false;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // CollapseButton
+            // 
+            this.CollapseButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CollapseButton.Image = ((System.Drawing.Image)(resources.GetObject("CollapseButton.Image")));
+            this.CollapseButton.Location = new System.Drawing.Point(6, 9);
+            this.CollapseButton.Name = "CollapseButton";
+            this.CollapseButton.Size = new System.Drawing.Size(35, 35);
+            this.CollapseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CollapseButton.TabIndex = 0;
+            this.CollapseButton.TabStop = false;
+            this.CollapseButton.Click += new System.EventHandler(this.CollapseButton_Click);
+            // 
+            // Container
+            // 
+            this.Container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Container.Location = new System.Drawing.Point(250, 50);
+            this.Container.Name = "Container";
+            this.Container.Size = new System.Drawing.Size(550, 299);
+            this.Container.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 349);
+            this.Controls.Add(this.Container);
+            this.Controls.Add(this.Header);
+            this.Controls.Add(this.MenuLeft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Header.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMaximize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMinimize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CollapseButton)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MenuLeft;
+        private System.Windows.Forms.Panel Header;
+        private System.Windows.Forms.PictureBox CollapseButton;
+        private System.Windows.Forms.Panel Container;
+        private System.Windows.Forms.PictureBox BtnClose;
+        private System.Windows.Forms.PictureBox BtnMin;
+        private System.Windows.Forms.PictureBox BtnMinimize;
+        private System.Windows.Forms.PictureBox BtnMaximize;
+    }
+}
+
---
+++ AppForm/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace AppForm
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void CollapseButton_Click(object sender, EventArgs e)
+        {
+            if(MenuLeft.Width==250)
+            {
+                MenuLeft.Width = 50;
+            }
+            else
+            {
+                MenuLeft.Width = 250;
+            }
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnMaximize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            BtnMinimize.Visible = true;
+            BtnMaximize.Visible = false;
+        }
+
+        private void BtnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal ;
+            BtnMinimize.Visible = false;
+            BtnMaximize.Visible = true;
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+}
